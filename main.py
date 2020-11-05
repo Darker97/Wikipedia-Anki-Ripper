6a30697ddce328754f910186a1b5e24f250ea2a4
Christian - Darker97 <christian@baltzer.de>
2020-11-05 22:40:33 +0100

    added Crawler

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import sys
 import urllib.request
-
-
-
+from bs4 import BeautifulSoup
+import genanki as anki
 #--------------------------------------
 
 
 
-# Webpage laden und Inhalt zurück geben
+# Webpage laden und Inhalt zuruck geben
 def WepRipper(Link):
     uf = urllib.request.urlopen(Link)
     html = uf.read()
-    
+
     return html
 
-# Überschriften und Inhalt rippen und als Array ausgeben + Titel der Seite als Überschrift
+# uberschriften und Inhalt rippen und als Array ausgeben + Titel der Seite als Uberschrift
 def Crawler(HTML):
-    pass
+    soup = BeautifulSoup(HTML)
+    Titel = soup.title.string
+    
+    #dt ist die Uberschrift
+    Header = soup.find_all("dt")
+    #dd der Text
+    Text = soup.find_all("dd")
+
+
+    print(len(Header))
+    print(len(Text))
+
+    Inhalt = []
+    for i in range(0,len(Header)-1):
+        if (Text[i] == "."):
+            continue
+
+        obj = {}
+        obj['Header'] = Header[i]
+        obj['Text'] = Text[i]
+        Inhalt.append(obj)
+    
+    return Titel, Inhalt
 
 
 # Anki Karten erstellen
@@ -32,14 +53,14 @@ def Error(Nachricht):
 # Main
 if __name__ == "__main__":
     Links = sys.argv[1:]
-    print(len(Links) + " -> Links gefunden")
+    print(str(len(Links)) + " -> Links gefunden")
     if (len(Links)==0):
         Error("Keine Links gefunden!")
 
     HTML = []
     for i in Links:
         HTML.append(WepRipper(i))
-    print(len(HTML) + " -> webpages geladen")
+    print(str(len(HTML)) + " -> webpages geladen")
     if(len(Links)==0):
         Error("Webpages konnten nicht geladen werden!")
     
