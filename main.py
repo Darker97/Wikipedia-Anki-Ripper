21e51711ec085282180f0946ef7ff6a5ce4dde4a
Christian - Darker97 <christian@baltzer.de>
2020-11-05 21:59:21 +0100

    Planung

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# Laden der Links und Ausgabe als Array voller Links + Name
+def LinksLaden(DateiName):
+    pass
+
+
+# Webpage laden und Inhalt zurück geben
+def WepRipper(Link):
+    pass
+
+# Überschriften und Inhalt rippen und als Array ausgeben + Titel der Seite als Überschrift
+def Crawler(HTML):
+    pass
+
+
+# Anki Karten erstellen
+def createKard(Name, Inhalt):
+    pass
+
+if __name__ == "__main__":
+    pass
